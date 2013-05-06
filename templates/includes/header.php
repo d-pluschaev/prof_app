4bfda1344bb5c0bc3d4b64ceb090d839cee4f43e
Vadim Romanenko <v.romanenko@intetics.com>
2013-05-06 12:08:02 +0300

    Include jQuery to the project. Add style to make jQuery modal windows not so huge.

--- templates/includes/header.php
+++ templates/includes/header.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset='utf-8'>
     <title><?=$this->title?></title>
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
     <link rel="stylesheet" type="text/css" href="css/style.css"/>
 </head>
 <body>
